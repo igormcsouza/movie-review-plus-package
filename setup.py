a14ceb0f57f3598e347ce6bc827437f308165e68
Igor <igormcsouza@gmail.com>
2020-07-05 23:01:40 -0300

    improved requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ setup(
     description='Process peoples review and classify as Positive or Negative',
     author='Igor Souza',
     url='https://igormcsouza.github.io',
-    install_requires=['numpy', 'keras', 'nltk', 'tqdm'],
+    install_requires=[
+        'numpy', 
+        'keras', 
+        'nltk>=3.5', 
+        'tqdm>=4.47.0',
+        'matplotlib',
+        'seaborn>=0.10.1'
+    ],
     packages=find_packages()
 )
