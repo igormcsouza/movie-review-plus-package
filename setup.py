c3ebc494c81946b6b084b6296d2da42fe968babc
Igor <igormcsouza@gmail.com>
2020-07-05 21:55:45 -0300

    added nltk as dependence

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='Process peoples review and classify as Positive or Negative',
     author='Igor Souza',
     url='https://igormcsouza.github.io',
-    install_requires=['numpy', 'keras'],
+    install_requires=['numpy', 'keras', 'nltk'],
     packages=find_packages()
 )
