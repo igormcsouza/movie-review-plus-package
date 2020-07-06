51024584987dc540d6af007c8da56856a1655650
Igor <igormcsouza@gmail.com>
2020-07-05 23:02:08 -0300

    new preprocessor

---
+++ classifier/preprocessors/remove_ponctuation_preprocessor.py
@@ -0,0 +1,21 @@
+from tqdm import tqdm
+from nltk import tokenize
+from string import punctuation
+
+from classifier.preprocessors import DefaultPreprocessors
+
+class RemovePonctuationPreprocessor(DefaultPreprocessors):
+
+    def apply_preprocessors(self, column):
+        punctuationTokenizer = tokenize.WordPunctTokenizer()
+
+        processed_sentece = list()
+        for each in tqdm(column):
+            filtered_sentence = list()
+            wordish = punctuationTokenizer.tokenize(each)
+            for item in wordish:
+                if item not in [p for p in punctuation]:
+                    filtered_sentence.append(item)
+            processed_sentece.append(' '.join(filtered_sentence))
+
+        return processed_sentece
