a094b200c6d8430b03a2a226488423fc0b3fb0c0
Igor <igormcsouza@gmail.com>
2020-07-07 22:10:55 -0300

    create classifier default

---
+++ classifier/classifier/__init__.py
@@ -0,0 +1,19 @@
+class DefaultClassifier():
+
+    def __init__(self, column, labels, max_features=100):
+        self._initialize_vectorizer_and_bag(column, labels, max_features)
+
+    def _initialize_vectorizer_and_bag(self, column, labels, max_features):
+        pass
+
+    def load(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def persist(self):
+        pass
